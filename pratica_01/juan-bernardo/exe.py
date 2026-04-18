2719874dcb62b328f01a4cd927aef6a6b22e9eb1
iz4nmura <bernardinho204@gmail.com>
2026-04-17 21:02:56 -0300

    atividade pra Anderson

---
+++ pratica_01/juan-bernardo/exe.py
@@ -0,0 +1,5 @@
+nome = input("Qual seu nome?")
+idade = int(input("Qual é o seu ano de nascimento?"))
+altura = float(input("Qual é a sua altura?"))
+
+print(f"Olá {nome}! Você tem {2026 - idade} anos e mede {altura} de altura\n")
